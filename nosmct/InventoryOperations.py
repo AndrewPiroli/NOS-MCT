6681861d7d0bf9c021c2a66d876443ff58b3bc12
Andrew Piroli <drewpiroli@gmail.com>
2022-06-15 13:51:43 -0400

    Rough in LibreNMS config file loading

--- nosmct/InventoryOperations.py
+++ nosmct/InventoryOperations.py
@@ -1,4 +1,5 @@
 import csv
+import json
 import pathlib
 from multiprocessing import Queue
 from typing import Iterator
@@ -26,3 +27,50 @@ def read_csv_config(filename: pathlib.Path, log_q: Queue) -> Iterator[dict]:
         log_q.put(f"debug read_config: header: {header}")
         for config_entry in reader:
             yield dict(zip(header, config_entry))
+
+
+def lnms_config_validate_and_set_defaults(config: dict) -> bool:
+    """
+    Validate and fill in missing defaults for the loaded LibreNMS config
+    """
+    if not isinstance(config, dict):
+        return False
+    if "host" not in config:
+        return False
+    if "protocol" not in config:
+        config["protocol"] = "https"
+    if (not isinstance(config["protocol"], str)) or config["protocol"].lower() not in (
+        "http",
+        "https",
+    ):
+        return False
+    else:
+        config["protocol"] = config["protocol"].lower()
+    if "port" not in config:
+        config["port"] = 80 if config["protocol"] == "http" else 443
+    if "tls_verify" not in config:
+        config["tls_verify"] = config["protocol"] == "https"
+    elif not isinstance(config["tls_verify"], bool):
+        return False
+    if "api_key" not in config:
+        return False
+    if not isinstance(config["api_key"], str):
+        return False
+
+
+def get_inventory_from_lnms(filename: pathlib.Path, log_q: Queue):
+    """
+    Retrieve an inventory from LibreNMS
+    The file passed is a json configuration file describing necessary info such as
+     - protocol
+     - host
+     - port
+     - API key
+     - filters to apply to the data
+     - transformations to apply to the data
+    """
+    with open(filename, "r") as config_file:
+        confdata = json.load(config_file)
+    if not lnms_config_validate_and_set_defaults(confdata):
+        raise RuntimeError
+    print(confdata)
