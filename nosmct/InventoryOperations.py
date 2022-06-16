03ba0774da2b151494b5dba50f2e75b2f0c2e0bb
Andrew Piroli <drewpiroli@gmail.com>
2022-06-16 09:16:04 -0400

    LibreNMS rework config validation

--- nosmct/InventoryOperations.py
+++ nosmct/InventoryOperations.py
@@ -30,6 +30,9 @@ def read_csv_config(filename: pathlib.Path, log_q: Queue) -> Iterator[dict]:
         for config_entry in reader:
             yield dict(zip(header, config_entry))
 
+lnms_config_exists = lambda key, config: (key in config)
+lnms_config_default = lambda key, default, config: config.__setitem__(key, default) if key not in config else None
+lnms_config_require = lambda key, valid_options, config: (key in config and config[key] in valid_options)
 
 def lnms_config_validate_and_set_defaults(config: dict) -> bool:
     """
@@ -37,28 +40,30 @@ def lnms_config_validate_and_set_defaults(config: dict) -> bool:
     """
     if not isinstance(config, dict):
         return False
-    if "host" not in config:
+    for required_key in ("host", "api_key", "filters", "translations"):
+        if not lnms_config_exists(required_key, config):
+            print(f"FIXMElog: Required config key: {required_key} not found in LibreNMS config")
+            return False
+    lnms_config_default("protocol", "https", config)
+    if not lnms_config_require("protocol", ("http", "https"), config):
+        print("Invalid protocol: " + config["protocol"])
         return False
-    if "protocol" not in config:
-        config["protocol"] = "https"
-    if (not isinstance(config["protocol"], str)) or config["protocol"].lower() not in (
-        "http",
-        "https",
-    ):
-        return False
-    else:
-        config["protocol"] = config["protocol"].lower()
-    if "port" not in config:
+    if not lnms_config_exists("port", config):
         config["port"] = 80 if config["protocol"] == "http" else 443
     elif not isinstance(config["port"], int):
         config["port"] = int(config["port"])
-    if "tls_verify" not in config:
-        config["tls_verify"] = config["protocol"] == "https"
-    elif not isinstance(config["tls_verify"], bool):
+    if not lnms_config_require("port", range(65536), config):
+        print("Invalid port no: " + config["port"])
         return False
-    if "api_key" not in config:
+    if not lnms_config_exists("tls_verify", config):
+        lnms_config_default("tls_verify", (config["protocol"] == "https"), config)
+    elif not lnms_config_require("tls_verify", (True, False), config):
         return False
     if not isinstance(config["api_key"], str):
+        return False
+    if not isinstance(config["filters"], dict):
+        return False
+    if not isinstance(config["translations"], dict):
         return False
     return True
 
